3453637b6859afc3de3d8ae571cb123773630e4d
Muhammad Hassaan Bin Tanveer <m.hassaanbintanveer@gmail.com>
2017-10-09 20:00:09 +0500

    code structure
    dfsdf

---
+++ main.js
@@ -0,0 +1,19 @@
+
+if(document.querySelector(".slider-container")) {
+  console.log("slider-enabled");
+  var slider = document.querySelector("#slider-fruit");
+  var controlLeft = document.querySelector("#slider-fruit .slider-control-left a");
+  var controlRight = document.querySelector("#slider-fruit .slider-control-right a");
+  controlLeft.addEventListener('click',slideLeft, true);
+  controlRight.addEventListener('click',slideRight, true);
+  console.log(controlRight);
+}
+
+function slideLeft(e){
+  e.preventDefault();
+  console.log("left");
+}
+function slideRight(e){
+  e.preventDefault();
+  console.log("right");
+}
